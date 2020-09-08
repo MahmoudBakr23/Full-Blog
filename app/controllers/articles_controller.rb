b32d8e8f3ec7e4d1b6e48e307674a3bd76b949c2
Mahmoud Bakr <mbakr6821@gmail.com>
2020-09-08 19:04:13 +0200

    create partial _form and modified the controller file

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,5 @@
 class ArticlesController < ApplicationController
+    include ArticlesHelper
 
     def index
         @articles = Article.all
@@ -8,5 +9,41 @@ class ArticlesController < ApplicationController
         @article = Article.find(
         params[:id]
         )
+    end
+
+    def new
+        @article = Article.new
+    end
+
+    def create
+        @article = Article.new(article_params)
+        @article.save
+        redirect_to article_path(@article)
+    end
+
+    def destroy
+        @article = Article.find(
+        params[:id]
+        )
+        @article.destroy
+        redirect_to articles_path
+    end
+
+    def edit
+        @article = Article.find(
+        params[:id]
+        )
+    end
+
+    def update
+        @article = Article.find(
+        params[:id]
+        )
+        @article.update(article_params)
+        redirect_to article_path(@article)
+    end
+
+    def article_params
+        params.require(:article).permit(:title, :body)
     end
 end
