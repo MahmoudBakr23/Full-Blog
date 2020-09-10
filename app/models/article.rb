413c5f00f3f966148a83e69bdc4a25b0baaf594a
Mahmoud Bakr <mbakr6821@gmail.com>
2020-09-10 09:10:16 +0200

    linked comment model with article

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,2 +1,3 @@
 class Article < ApplicationRecord
+    has_many :comments, class_name: "comment", foreign_key: "reference_id"
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_08_131354) do
+ActiveRecord::Schema.define(version: 2020_09_10_055234) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "comments", force: :cascade do |t|
+    t.string "authour_name"
+    t.text "body"
+    t.integer "article_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["article_id"], name: "index_comments_on_article_id"
   end
 
 end
